f622109bdea3ecc4b7ad4d8c0eff321892845594
web3sea <yanji.notification@gmail.com>
2025-07-13 23:36:29 +0700

    feat(config): :wrench: update config

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,6 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -15,7 +17,30 @@ module.exports = defineConfig({
   },
   admin: {
     disable: process.env.MEDUSA_ADMIN_DISABLE === "true" || false,
-    backendUrl: process.env.MEDUSA_ADMIN_BACKEND_UR || "http://localhost:9000",
+    backendUrl: process.env.MEDUSA_ADMIN_BACKEND_URL || "http://localhost:9000",
     path: `/${process.env.MEDUSA_ADMIN_PATH?.replace(/^\//, '') || 'app'}`,
-  }
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+  ],
+
 })
